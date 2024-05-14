01f92da88beaf0446cbb18468d729393ff4f3b70
adsorptionenthalpy <androulidakisa@gmail.com>
2024-05-14 13:10:56 -0600

    fixes

--- godot/scenes/Autoloads/global_values.gd
+++ godot/scenes/Autoloads/global_values.gd
@@ -4,3 +4,10 @@ extends Node
 
 func _ready() -> void:
 	DisplayServer.window_set_min_size(Vector2i(480, 360))
+	
+	if OS.has_feature("android"):
+		get_window().content_scale_factor = 2
+		if get_window().size.y >= 1440:
+			get_window().content_scale_factor = 2.25
+		elif get_window().size.y >= 2160:
+			get_window().content_scale_factor = 2.75
